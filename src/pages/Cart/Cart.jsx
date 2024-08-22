68f54acff27da1fda41529aab483eef76bb22187
Agung Rachmad Prihanto <agungrachmadprihanto@gmail.com>
2024-08-22 22:14:10 +0700

    update ui

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../Context/StoreContext";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
   const { cartItem, food_list, removeFromCart, getTotalCartAmount } =
     useContext(StoreContext);
+
+  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -52,15 +55,17 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>{2}</p>
+              <p>{getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount() + 2}</b>
+              <b>
+                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
             </div>
           </div>
-          <button>Checkout</button>
+          <button onClick={() => navigate("/order")}>Checkout</button>
         </div>
         <div className="cart-promocode">
           <div>
--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
-import './PlaceOrder.css';
+import React, { useContext } from "react";
+import "./PlaceOrder.css";
+import { StoreContext } from "../../Context/StoreContext";
 
 const PlaceOrder = () => {
-    return (
-        <div className='placeorder'>
-            
+  const { getTotalCartAmount } = useContext(StoreContext);
+
+  return (
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name"></input>
+          <input type="text" placeholder="Last Name"></input>
         </div>
-    );
+        <input type="email" placeholder="email" />
+        <input type="text" placeholder="Streeet" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City"></input>
+          <input type="text" placeholder="State"></input>
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder="Zip Code"></input>
+          <input type="text" placeholder="Country"></input>
+        </div>
+        <input type="number" placeholder="WhastApp" />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>{getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>
+                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              </b>
+            </div>
+          </div>
+          <button onClick={() => navigate()}>Checkout</button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default PlaceOrder;
-
